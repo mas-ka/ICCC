29e7e7e489bed62407a5d9365049b83aeb710771
kawamoto <kawamoto@saga-ls.jp>
2024-05-08 17:02:18 +0900

    動作周波数を1MHzにし、ブザー音の周波数もちょっとだけ変更した。

--- iccc.ino
+++ iccc.ino
@@ -17,6 +17,8 @@
 #define TEST_PIN  A2  // テストリード電圧（PB4はA2）
 
 // 変数
+#define TONE_HIGH 2888 // 高音の周波数 (@1MHz)
+#define TONE_LOW  736  // 低音の周波数 (@1MHz)
 unsigned int count_sw; // スイッチ押下カウンタ
 unsigned long ms_last_event; // 最後にテスター検知をした時刻
 double adc_test; // テスターのADC値
@@ -27,7 +29,7 @@ void setup() {
   //mySerial.println("ICCC(ATtiny85) start");
   
   // ピンアサイン
-  //pinMode(SW_PIN, INPUT);
+  pinMode(SW_PIN, INPUT);
   pinMode(EN_PIN, OUTPUT);
   
   // ADC設定
@@ -46,8 +48,8 @@ void loop() {
   // バッテリー残量のチェック
   if (readVBatt() < 0.9) {// バッテリー残量が0.9V未満となった
     noTone(BZ_PIN); delay(200); // 一旦ブザーを止める
-    tone(BZ_PIN, 554, 240); delay(250); tone(BZ_PIN, 932, 240); delay(250); 
-    tone(BZ_PIN, 554, 240); delay(250); tone(BZ_PIN, 932, 240); delay(250); 
+    tone(BZ_PIN, TONE_LOW, 240); delay(250); tone(BZ_PIN, TONE_HIGH, 240); delay(250); 
+    tone(BZ_PIN, TONE_LOW, 240); delay(250); tone(BZ_PIN, TONE_HIGH, 240); delay(250); 
     noTone(BZ_PIN); // 一旦ブザーを止める
     digitalWrite(EN_PIN, LOW); // 電源許可を停止
     while(1){;} // 停止まで無期限待機
@@ -60,7 +62,7 @@ void loop() {
     count_sw++; // 押下回数をインクリメント
   } else { // 5回以上押し続けられてるので強制電源オフ
     noTone(BZ_PIN); delay(200); // 一旦ブザーを止める
-    tone(BZ_PIN, 2688, 1000); delay(1250); // 高音で1秒発音 
+    tone(BZ_PIN, TONE_HIGH, 1000); delay(1250); // 高音で1秒発音 
     noTone(BZ_PIN); // 一旦ブザーを止める
     digitalWrite(EN_PIN, LOW); // 電源許可を停止
     while(1){;} // 停止まで無期限待機
@@ -69,7 +71,7 @@ void loop() {
   // 自動電源オフのチェック
   if (millis() - ms_last_event > 30000) { // 最後のテスター検知から30秒以上経過した
     noTone(BZ_PIN); delay(200); // 一旦ブザーを止める
-    tone(BZ_PIN, 2688, 1000); delay(1250); // 高音で1秒発音 
+    tone(BZ_PIN, TONE_HIGH, 1000); delay(1250); // 高音で1秒発音 
     noTone(BZ_PIN); // 一旦ブザーを止める
     digitalWrite(EN_PIN, LOW); // 電源許可を停止
     while(1){;} // 停止まで無期限待機
@@ -78,17 +80,17 @@ void loop() {
   // テスター検知のチェック
   adc_test = readADCTest();
   if (adc_test < 2.0) { // ADC値が2（大体10Ω）未満
-    tone(BZ_PIN, 2688); // 高音連続音
+    tone(BZ_PIN, TONE_HIGH); // 高音連続音
     ms_last_event = millis(); // テスター検知時刻を更新
   } else if (adc_test < 10.0) { // ADC値が2（10Ω）以上 10（50Ω）未満
     noTone(); // ブザーを止める
-    tone(BZ_PIN, 928, 200); delay(250); // 中音断続音
+    tone(BZ_PIN, TONE_LOW, 200); delay(250); // 低音断続音
     ms_last_event = millis(); // テスター検知時刻を更新
   } else { // 絶縁状態
     noTone(); // ブザーを止める
   }
 
-  delay(100);
+  delay(50);
 }
 
 /*
